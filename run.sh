fa9a31d3c174e57e56e195c5d647cc3af11c6bb2
c-graham-qut <102006352+c-graham-qut@users.noreply.github.com>
2022-09-07 16:40:59 +1000

    Add files via upload

--- run.sh
+++ run.sh
@@ -3,4 +3,15 @@ sudo add-apt-repository ppa:ubunutu-toolchain-r/test
 sudo apt-get update
 cp -avr $(pwd)/Spatial-Index-Components/include/spatialindex /usr/include
 rsync -a $(pwd)/Spatial-Index-Components/lib/ /usr/lib
+sudo apt-get install python3
+sudo apt install git python3-pip libxcb-xinerama0
+sudo pip3 install --upgrade pip
+sudo pip3 install cflib
+sudo pip3 install pynput
+sudo pip3 install keyboard
+sudo pip3 install numpy
 sudo make all
+sudo git clone https://github.com/bitcraze/crazyflie-clients-python
+sudo cd crazyflie-clients-python
+sudo pip3 install -e .
+
